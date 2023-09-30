87c26e5140e4bcd022b846fc8761f0d28cb6c99a
Boris Vassilev <boris.n.vassilev@gmail.com>
2023-09-30 17:54:10 +0300

    Week 1 problems and solutions

---
+++ week_01/problem_1.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	cout << "Hello, FMI!" << endl;
+	return 0;
+}
---
+++ week_01/problem_3.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <math.h>
+
+using namespace std;
+
+int main() {
+	float a, b;
+	cin >> a >> b;
+	float ans = sqrt(a * a + b * b);
+	cout << ans << endl;
+	return 0;
+}
